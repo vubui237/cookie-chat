b06cecbd90ed3d571646aeb0e9973dbd9a5f5143
vubui237 <vtb0977@gmail.com>
2017-07-19 15:54:38 -0500

    Added colors..

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -5,9 +5,11 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
   $scope.getMessages = function() {
     $scope.messages = messageService.getMessages().then(function(response){
     $scope.messages = response.data;
+    console.log($scope.messages);
 
     })
   }
+  
   //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
@@ -27,9 +29,9 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
 
   //uncomment this code when your getMessages function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-   setInterval(function(){
+  // setInterval(function(){
     $scope.getMessages();
 
-  }, 9999991500)
+  //}, 1500)
 })
 
